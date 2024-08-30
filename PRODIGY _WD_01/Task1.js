10b9e14efaa696db0df271958f56efe803ab081a
asbru282222j284 <144264189+asbru282222j284@users.noreply.github.com>
2024-08-30 20:24:55 +0530

    Add files via upload

---
+++ PRODIGY _WD_01/Task1.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector("#menu-icon");
+const navbar = document.querySelector("#navbar");
+menu.addEventListener("click", function () {
+navbar.classList.toggle("active");
+});
+window.onscroll = () => {
+navbar.classList.remove("active");
+};
